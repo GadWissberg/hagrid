103a512dfbeb5bb76b3f17200443facae6b4b6ab
bar <bar@root.co.il>
2020-09-09 01:28:50 +0300

    Server: small code refactor for server - UDP still not working (need some changes)
    Client:
    added support for TCP/UDP ipv4+ipv6 and UDS

--- client/main.py
+++ client/main.py
@@ -1,17 +1,65 @@
 import socket
+import select
+import sys
 
+# some constants
+USE_PORT    = 32007
+BUFFER_SIZE = 1024
 
 def main():
-    # TCP based client socket
-    echoClient = socket.socket()
-    echoClient.connect((socket.gethostbyname(socket.gethostname()), 32007))
-    chat_id = input("Enter chat ID: ")
-    echoClient.send(chat_id.encode())
-    while True:
-        msgReceived = echoClient.recv(1024)
-        print("At client: %s" % msgReceived.decode())
-        echoClient.send(input().encode())
+    protocol = input("Please choose protocol (TCP / UDP / UDS): ").lower()
+    if protocol == "tcp" or protocol == "udp":
+        sockFamily = socket.AF_INET
+        sockType = socket.SOCK_STREAM if protocol == "tcp" else socket.SOCK_DGRAM
+        host = input("Please insert server IP address: ")
+        if ":" in host:
+            sockFamily = socket.AF_INET6
+        port = int(input("Please insert server port: "))
+    elif protocol == "uds":
+        sockFamily = socket.AF_UNIX
+        sockType = socket.SOCK_STREAM
+        sockFile = input("Please insert path to server socket file: ")
+    else:
+        print("Unknown protocol")
+        return
 
+    try:
+        server = socket.socket(sockFamily, sockType)
+        if protocol == "uds":
+            print("UDS: connecting to "+sockFile)
+            server.connect(sockFile)
+        else:
+            print("connecting to " + host + ":"+str(port))
+            server.connect((host, port))
+
+    except Exception as e:
+        print("Can't connect to server, please make sure that:\n"
+              "1. server is running\n"
+              "2. you have provided right connection information")
+
+        print("Exception: "+str(e))
+        return
+
+
+    # list of available input streams
+    streams = [sys.stdin, server]
+
+    # TODO:
+    # allow client to choose his own nickname
+    #name = input("Enter your name: ")
+
+    while True:
+        readSock, writeSock, errSock = select.select(streams, [], [])
+        for sock in readSock:
+            if sock == server:
+                # message received from server -> print to client
+                message = sock.recv(BUFFER_SIZE)
+                print(message.decode())
+            else:
+                # input from client -> send to server
+                message = sys.stdin.readline()
+                server.send(message.encode())
+                print("<You> "+message)
 
 if __name__ == '__main__':
     main()
--- server/main.py
+++ server/main.py
@@ -1,14 +1,18 @@
 import socket
 import sys
 import os
-from Chat import Chat
+from threading import *
+
+# some constants
+USE_PORT        = 32007 # TODO: remove this and allow user to choose his own port
+BUFFER_SIZE     = 1024
+SOCKET_PATH     = "chat.sock" # TODO: same as above
+BACKLOG_SIZE    = 10
+
+# global vars
+clients = [] # list of all connected clients
 
 def main():
-    # some constants
-    USE_PORT        = 32007
-    SOCKET_PATH     = "chat.sock"
-    BACKLOG_SIZE    = 10
-
     # Dealing with command line arguments
     argsNum = len(sys.argv)
     if argsNum < 2:
@@ -20,94 +24,137 @@ def main():
     for i in range(0, argsNum):
         sys.argv[i] = str(sys.argv[i]).lower()
 
-    # TCP based server
-    if sys.argv[1] == 'tcp':
-        ipv = IPversion()
-        if ipv is False:
-            return
+    # TCP/UDP based server
+    if sys.argv[1] == 'tcp' or sys.argv[1] == 'udp':
+        sockType = socket.SOCK_STREAM if sys.argv[1] == "tcp" else socket.SOCK_DGRAM
 
-        if ipv == 4:
-            useType = socket.AF_INET
-        else:
-            useType = socket.AF_INET6
+        try:
+            if sys.argv[2] != 'ipv4' and sys.argv[2] != 'ipv6':
+                print("You must select IP version!\nPlease run: " + sys.argv[0] + " " + sys.argv[1] + " ipv4 or ipv6")
+                return False
 
-        ipaddr = getIPaddr(useType, USE_PORT)
+            sockFamily = socket.AF_INET if sys.argv[2] == 'ipv4' else socket.AF_INET6
+        except IndexError:
+            print("You must select IP version!\nPlease run: " + sys.argv[0] + " " + sys.argv[1] + " ipv4 or ipv6")
+            return False
 
-        # create socket
-        server_socket = socket.socket(useType, socket.SOCK_STREAM)
-        server_socket.bind((ipaddr, USE_PORT))
-        server_socket.listen(BACKLOG_SIZE)
-
-        print("Server is listening for incoming connections, " + ipaddr + ":" + str(USE_PORT) + " (TCP)")
-
-        while True:
-            connection, client_address = server_socket.accept()
-            Chat(connection, client_address)
-
-    # UDP based server
-    elif sys.argv[1] == 'udp':
-        ipv = IPversion()
-        if ipv is False:
-            return
-
-        if ipv == 4:
-            useType = socket.AF_INET
-        else:
-            useType = socket.AF_INET6
-
-        ipaddr = getIPaddr(useType, USE_PORT)
-
-        # create socket
-        server_socket = socket.socket(useType, socket.SOCK_DGRAM)
-        server_socket.bind((ipaddr, USE_PORT))
-
-        print("Server is listening for incoming connections, " + ipaddr + ":" + str(USE_PORT) + " (UDP)")
-
-        # FYI
-        # UDP protocol DO NOT listen to incoming messages, instead of socket.listen() we should just receive messages with socket.revcfrom()
-        while True:
-            data, addr = server_socket.recvfrom(1024) # 1024 is selected buffer size
-            print("message: " + data + " from: " + addr)
+        ipaddr = getIPaddr(sockFamily, USE_PORT)
+        sockBind = (ipaddr, USE_PORT)
 
     # UDS based server
     elif sys.argv[1] == 'uds':
-        # create socket
-
         # remove socket file if already exists
         try:
             os.unlink(SOCKET_PATH)
         except OSError:
             raise
 
-        server_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        server_socket.bind(SOCKET_PATH)
-        server_socket.listen(BACKLOG_SIZE)
-
-        print("Server is listening for incoming connections, " + str(SOCKET_PATH) + " (UDS)")
-
-        while True:
-            connection, client_address = server_socket.accept()
-            Chat(connection, client_address)
+        sockFamily = socket.AF_UNIX
+        sockType = socket.SOCK_STREAM
+        sockBind = SOCKET_PATH
 
     else:
         print("selected protocol " + sys.argv[1] + " is NOT supported")
         return
 
-def IPversion():
-    try:
-        if sys.argv[2] == 'ipv4':
-            return 4
+    # create server socket
+    server_socket = socket.socket(sockFamily, sockType)
 
-        if sys.argv[2] == 'ipv6':
-            return 6
-    except IndexError:
-        print("You should select IP version!\nPlease run: " + sys.argv[0] + " " + sys.argv[1] + " ipv4 or ipv6")
-        return False
+    if sys.argv[1] == 'tcp':
+        # prevent "Address already in use" exception because of TIME_WAIT state
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    server_socket.bind(sockBind)
+
+    if sys.argv[1] == 'tcp' or sys.argv[1] == 'uds':
+        server_socket.listen(BACKLOG_SIZE)
+
+    print("Server is listening for incoming connections")
+
+    if sys.argv[1] == "uds":
+        print("socket file path: "+sockBind)
+    else:
+        print(ipaddr + ":" + str(USE_PORT) + " (" + sys.argv[1] + ")")
+
+    while True:
+        if sys.argv[1] == 'udp':
+            # UDP protocol DO NOT listen to incoming messages, instead of socket.listen() we should just receive messages with socket.revcfrom()
+            data, addr = server_socket.recvfrom(BUFFER_SIZE)
+        else:
+            connection, clientIP = server_socket.accept()
+
+        # TODO:
+        # handle UDP connection based, as it's not possible to use the same logic as TCP/UDS uses
+
+        clients.append(connection)
+
+        if sys.argv[1] == 'uds':
+            print("client connected to this server " + str(len(clients)) + " clients are connected)")
+            clientThread(connection, "")
+        else:
+            print("client " + clientIP[0] + " connected to this server (" + str(len(clients)) + " clients are connected)")
+            clientThread(connection, clientIP[0])
 
 def getIPaddr(ipv, port):
     hostname = socket.getfqdn()
     ipinfo = socket.getaddrinfo(hostname, port, ipv)
     return ipinfo[0][4][0]
+
+def sendMessage(msg, srcClient, IP):
+    # broadcast message to all available clients, except the one who send the message
+
+    # TODO: fix this if condition (do not work)
+    # msg type is "bytes" class
+    if msg == "":
+        return False
+
+    # it's possible to print here (to server console) "received message XXX from client YYY"
+
+    for client in clients:
+        # do not send to source client
+        if client != srcClient:
+            try:
+                msg = "<"+IP+"> "+msg.decode()
+                msg = msg.encode()
+                client.send(msg)
+            except Exception as e:
+                print("sendMessage Exception: "+str(e))
+                client.close()
+                dropConnection(client)
+
+def dropConnection(client):
+    # remove client from our clients list
+    if client not in clients:
+        return False
+
+    print(type(client))
+    print(client)
+    print("client lost connection")
+    return clients.remove(client)
+
+
+class clientThread(Thread):
+    def __init__(self, client, ipaddr):
+        Thread.__init__(self)
+        self.client = client
+        self.ipaddr = ipaddr
+        #self.name = "" TODO: add support for client name/nick
+        self.start()
+
+    def run(self):
+        self.client.send(b"Welcome to chat server!")
+
+        while True:
+            try:
+                message = self.client.recv(BUFFER_SIZE)
+                sendMessage(message, self.client, self.ipaddr)
+            except Exception as e:
+                print("clientThread Exception: "+str(e))
+
+                # TODO:
+                # disconnect client (connection timed-out / aborted / something happened)
+                # stop thread and handle everything else
+                continue
 
 if __name__ == '__main__':
     main()
